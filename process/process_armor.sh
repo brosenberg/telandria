300c75cfc6a394ab3d25da1b81a23c5ff249f57a
brosenberg <brosenberg@pistoncloud.com>
2014-09-22 23:25:32 -0700

    Add hitpoints and hardness for armors

--- process/process_armor.sh
+++ process/process_armor.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 echo "{\"armor\":{"
+sed '/^$/d' armors  | \
 awk '{print "\"" $1 "\": {\n" \
             "\t\"cost\": \"" $2 "\",\n" \
             "\t\"armor/shield bonus\": \"" $3 "\",\n" \
+            "\t\"hitpoints\": \"" $3*5 "\",\n" \
+            "\t\"hardness\": \"" $10 "\",\n" \
             "\t\"max dex\": \"" $4 "\",\n" \
             "\t\"armor check\": \"" $5 "\",\n" \
             "\t\"arcane spell failure\": \"" $6 "\",\n" \
             "\t\"speed (30 ft)\": \"" $7 "\",\n" \
             "\t\"speed (20 ft)\": \"" $8 "\",\n" \
             "\t\"weight\": \"" $9 "\",\n" \
-            "\t\"type\": \"" $10 "\"\n" \
-    "},"}' armors | sed 's/^/\t/;s/_/ /g;$s/,//'
+            "\t\"type\": \"" $11 "\"\n" \
+    "},"}' | \
+sed 's/^/\t/;s/_/ /g;$s/,//'
 echo "}"
